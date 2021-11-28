0b19a14d966780f0d2813be70f2b484b734081c2
tst32 <tst32@users.noreply.github.com>
2021-11-28 20:14:30 +0300

    read env vars from process.env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import {ENVIRONMENT} from "./globals";
+import {ENVIRONMENT}  from "./globals";
 import {ANOTHER_VARIABLE} from "./globals";
-
-export class Utils {
-    public static isProduction(): boolean {
+ export class Utils {
+    public env: string; 
+    public jj: any;
+    constructor() {
+        this.env = ENVIRONMENT; //this is undefined
+        this.env = `${process.env.ENVIRONMENT}` //read a value from environment 
+      };
+    public static isProduction(): boolean { 
         if (ANOTHER_VARIABLE === 'some value') {
             const jj = 1;
         }
+
         return ENVIRONMENT === 'production';
     }
 }
+
+const utils = new Utils()
+console.log(utils.env) 
